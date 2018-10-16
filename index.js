97c8d8e944ac4ca53cfd48e7e574300d4374450d
EC2 Default User <ec2-user@ip-172-31-38-97.us-east-2.compute.internal>
2018-10-16 22:47:21 +0000

    Moving up

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl',
     function($scope, $http) {
-        $scope.foodItems = [];
-        $scope.onup = function(form) {
-            console.log(form);
-            var url = "https://www.food2fork.com/api/get?key=ee9f9b47f98d89aa1add7b8fb99c2dad&rId=35382";
-            
+        var apiKey = "5bc65c3f863c5056eefc046d1bad46f1cb5dac528f1e8";
+
+        $scope.onup = function() {
+            $scope.websiteFinePrint = "";
+            console.log($scope.url);
+            var url = "https://api.linkpreview.net/?key=" + apiKey + "&q=" + $scope.url;
+
             $http.get(url).then(function(response) {
-                console.log(response);
-                $scope.recipe = response.data;
-                
-                $scope.foodItems = response.data.recipe.ingredients;
-                
-            });
+                    console.log(response);
+
+                    $scope.websiteTitle = response.data.title;
+                    $scope.websiteDescription = response.data.description;
+                    if (response.data.image != "") {
+                        $scope.websitePreview = response.data.image;
+                    } else {
+                        $scope.websitePreview = "Stick.jpg";
+                    }
+                    $scope.websiteURL = response.data.url;
+                })
+                .catch((err => {
+                    console.log("Woops");
+                    $scope.websiteName = "Dog.jpg";
+                    
+                    $scope.websiteTitle = "You've summoned the wrath of the admins";
+                    $scope.websiteDescription = "This website really doesn't want you to see what's inside";
+                    $scope.websitePreview = "Dog.jpg";
+                    $scope.websiteURL = "https://www.youtube.com/watch?v=8uXrgv1xIFk";
+                    $scope.websiteFinePrint = "(Or you just put in a weird address)";
+                }));
             console.log("in onup");
-
-
-
-
         };
 
-        
+
 
     });
